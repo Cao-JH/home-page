18672385fc6f1359092b2ac712062b0380fc1938
曹嘉宏 <caojh@zyws.cn>
2024-02-22 11:12:25 +0800

    收藏夹页面样式 + 拖拽功能

--- src/views/FavoritePage/index.vue
+++ src/views/FavoritePage/index.vue
@@ -1,72 +1,45 @@
 <template>
   <div id="favorite-content">
     <draggable class="list-group" 
-      :move="onMoveCallback"
-      v-bind="dragOptions"
-      @change="log"
-      @start="isDragging = true"
+      :list="favoriteList"
+      item-key="id"
+      :animation="500"
+      :forceFallback="true"
+      @change="onMoveCallback"
+      :move="getData"
+      @start="isDragging = false"
       @end="isDragging = false">
-      <template #item="{dragOptions}">
+      <template #item="{ element }">
         <div>
           <div class="favorite-boxs">
             <div class="favorite-title">
-              <span class="title">这是标题</span>
+              <span class="title">{{ element.label }}</span>
               <div></div>
             </div>
-            <div class="favorite-box">
-              <div class="avatar-box">
-                <img src="@/assets/img/avatar.jpg" alt="" />
-              </div>
-              <div class="text-box">
-                <div class="text-top">
-                  <div class="text-top-right">最常222123213去</div>
-                  <div class="text-top-left">
-                    <div class="point favorite">
-                      <span class="icon-shoucang iconfont"></span>
-                    </div>
-                    <div class="point edit">
-                      <span class="icon-bianji iconfont"></span>
-                    </div>
-                    <div class="point delete">
-                      <span class="icon-shanchu iconfont"></span>
+            <div class="favorites-box">
+              <div class="favorite-box" v-for="item in element.favorites" :key="item.id">
+                <div class="avatar-box">
+                  <img src="@/assets/img/avatar.jpg" alt="" />
+                </div>
+                <div class="text-box">
+                  <div class="text-top">
+                    <div class="text-top-right">{{item.title}}</div>
+                    <div class="text-top-left">
+                      <div class="point favorite">
+                        <span class="icon-shoucang iconfont"></span>
+                      </div>
+                      <div class="point edit">
+                        <span class="icon-bianji iconfont"></span>
+                      </div>
+                      <div class="point delete">
+                        <span class="icon-shanchu iconfont"></span>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div class="text-bottom">
-                  <div class="text-direction">
-                    记录卡恩SV老接口萨恩反馈拉我没拿到克拉妹那我蓝开拿饭卡里稳定卡位面
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="favorite-boxs">
-            <div class="favorite-title">
-              <span class="title">这是标题</span>
-              <div></div>
-            </div>
-            <div class="favorite-box">
-              <div class="avatar-box">
-                <img src="@/assets/img/avatar.jpg" alt="" />
-              </div>
-              <div class="text-box">
-                <div class="text-top">
-                  <div class="text-top-right">最常222123213去</div>
-                  <div class="text-top-left">
-                    <div class="point favorite">
-                      <span class="icon-shoucang iconfont"></span>
+                  <div class="text-bottom">
+                    <div class="text-direction">
+                      {{item.description}}
                     </div>
-                    <div class="point edit">
-                      <span class="icon-bianji iconfont"></span>
-                    </div>
-                    <div class="point delete">
-                      <span class="icon-shanchu iconfont"></span>
-                    </div>
-                  </div>
-                </div>
-                <div class="text-bottom">
-                  <div class="text-direction">
-                    记录卡恩SV老接口萨恩反馈拉我没拿到克拉妹那我蓝开拿饭卡里稳定卡位面
                   </div>
                 </div>
               </div>
@@ -79,32 +52,178 @@
 </template>
 
 <script setup>
-import { ref, reactive, computed } from "vue";
+import { ref } from "vue";
 import draggable from 'vuedraggable';
 
-const dragOptions = computed(() => { // 拖拽动画效果
-  return {
-    animation: 0,
-    group: 'description',
-    disabled: false,
-    ghostClass: 'ghost',
-  }
-})
+const favoriteList = ref([
+  {
+    index: 0,
+    id: 'aaa',
+    label: '这是标题1',
+    favorites: [
+      {
+        id: 1,
+        imgSrc: '',
+        title: '最常222123213去',
+        description: '记录卡恩SV老接口萨恩反馈拉我没拿到克拉妹那我蓝开拿饭卡里稳定卡位面',
+      },
+      {
+        id: 2,
+        imgSrc: '',
+        title: 'aaaa',
+        description: 'vsdawdawd',
+      },
+      {
+        id: 3,
+        imgSrc: '',
+        title: 'bbbb',
+        description: 'adwa',
+      },
+      {
+        id: 4,
+        imgSrc: '',
+        title: 'cccc',
+        description: 'vsdawdawd',
+      },
+      {
+        id: 5,
+        imgSrc: '',
+        title: '最常222123213去',
+        description: '记录卡恩SV老接口萨恩反馈拉我没拿到克拉妹那我蓝开拿饭卡里稳定卡位面',
+      },
+      {
+        id: 6,
+        imgSrc: '',
+        title: 'aaaa',
+        description: 'vsdawdawd',
+      },
+      {
+        id: 7,
+        imgSrc: '',
+        title: 'bbbb',
+        description: 'adwa',
+      },
+      {
+        id: 8,
+        imgSrc: '',
+        title: 'cccc',
+        description: 'vsdawdawd',
+      },
+      {
+        id: 9,
+        imgSrc: '',
+        title: '最常222123213去',
+        description: '记录卡恩SV老接口萨恩反馈拉我没拿到克拉妹那我蓝开拿饭卡里稳定卡位面',
+      },
+      {
+        id: 10,
+        imgSrc: '',
+        title: 'aaaa',
+        description: 'vsdawdawd',
+      },
+      {
+        id: 11,
+        imgSrc: '',
+        title: 'bbbb',
+        description: 'adwa',
+      },
+      {
+        id: 12,
+        imgSrc: '',
+        title: 'cccc',
+        description: 'vsdawdawd',
+      },
+    ]
+  },
+  {
+    index: 1,
+    id: 'bbb',
+    label: '这是标题2',
+    favorites: [
+      {
+        id: 1,
+        imgSrc: '',
+        title: '最常222123213去',
+        description: '记录卡恩SV老接口萨恩反馈拉我没拿到克拉妹那我蓝开拿饭卡里稳定卡位面',
+      }
+    ]
+  },
+  {
+    index: 2,
+    id: 'ccc',
+    label: '这是标题3',
+    favorites: [
+      {
+        id: 1,
+        imgSrc: '',
+        title: '最常222123213去',
+        description: '记录卡恩SV老接口萨恩反馈拉我没拿到克拉妹那我蓝开拿饭卡里稳定卡位面',
+      }
+    ]
+  },
+  {
+    index: 3,
+    id: 'ccc',
+    label: '这是标题4',
+    favorites: [
+      {
+        id: 1,
+        imgSrc: '',
+        title: '最常222123213去',
+        description: '记录卡恩SV老接口萨恩反馈拉我没拿到克拉妹那我蓝开拿饭卡里稳定卡位面',
+      }
+    ]
+  },
+  {
+    index: 4,
+    id: 'ccc',
+    label: '这是标题5',
+    favorites: [
+      {
+        id: 1,
+        imgSrc: '',
+        title: '最常222123213去',
+        description: '记录卡恩SV老接口萨恩反馈拉我没拿到克拉妹那我蓝开拿饭卡里稳定卡位面',
+      }
+    ]
+  },
+  {
+    index: 5,
+    id: 'ccc',
+    label: '这是标题6',
+    favorites: [
+      {
+        id: 1,
+        imgSrc: '',
+        title: '最常222123213去',
+        description: '记录卡恩SV老接口萨恩反馈拉我没拿到克拉妹那我蓝开拿饭卡里稳定卡位面',
+      }
+    ]
+  },
+])
 
-const log = function(evt) {
-  window.console.log(evt);
-}
-const onMoveCallback = (evt, originalEvent) => {
-  console.log(evt)
-  // this.currentTask = evt.draggedContext.element
-  // this.current = +evt.to.dataset.index
-}
+const onMoveCallback = () => {
+  const items = favoriteList.value;
+  let index = 0;
+  items.forEach(item => {
+    item.index = index;
+    index++;
+  })
+};
+
+const getData = (val) => {
+  console.log(val);
+  // console.log(favoriteList.value, 'favoriteList');
+};
 
 </script>
 
 <style scoped lang="less">
 #favorite-content {
-  padding: 20px 30px;
+  padding: 20px 20px;
+  height: 100%;
+  width: 100%;
+  overflow: auto;
 
   .favorite-boxs {
     margin: 0 0 20px;
@@ -118,118 +237,126 @@ const onMoveCallback = (evt, originalEvent) => {
       font-style: italic;
       color: @titleFont;
     }
-  
-    .favorite-box {
-      width: 210px;
-      height: 80px;
-      padding: 10px;
-      border-radius: 10px;
-      background: @background;
-      box-shadow: 5px 5px 15px #bebebe, -5px -5px 15px #ffffff;
+
+    .favorites-box {
       display: flex;
-      align-items: center;
-      transition: transform 0.5s ease;
-      cursor: pointer;
-  
-      .avatar-box {
-        width: 50px;
-        height: 50px;
-        margin-right: 10px;
-  
-        img {
-          border-radius: 50%;
-          width: 100%;
-          height: 100%;
-        }
-      }
-  
-      .text-box {
-        height: 60px;
+      flex-direction: row;
+      flex-wrap: wrap;
+      justify-content: flex-start;
+      
+      .favorite-box {
+        width: 210px;
+        height: 80px;
+        padding: 10px;
+        margin: 10px;
+        border-radius: 10px;
+        background: @background;
+        box-shadow: 5px 5px 15px #bebebe, -5px -5px 15px #ffffff;
         display: flex;
-        flex-direction: column;
-        justify-content: flex-start;
-  
-        .text-top {
-          width: 130px;
-          height: 20px;
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
-  
-          .text-top-right {
-            width: 80px;
-            font-size: 13px;
-            white-space: nowrap;
-            overflow: hidden;
-            text-overflow: ellipsis;
+        align-items: center;
+        transition: transform 0.5s ease;
+        cursor: pointer;
+    
+        .avatar-box {
+          width: 50px;
+          height: 50px;
+          margin-right: 10px;
+    
+          img {
+            border-radius: 50%;
+            width: 100%;
+            height: 100%;
           }
-  
-          .text-top-left {
-            width: 50px;
+        }
+    
+        .text-box {
+          height: 60px;
+          display: flex;
+          flex-direction: column;
+          justify-content: flex-start;
+    
+          .text-top {
+            width: 130px;
+            height: 20px;
             display: flex;
-            flex-direction: row;
-  
-            .point {
-              width: 12px;
-              height: 12px;
-              margin-left: 4px;
-              border-radius: 50%;
-              text-align: center;
-              transition: transform 0.5s ease;
-  
-              .iconfont {
-                opacity: 0;
-                font-size: 9px !important;
-                line-height: 12px;
-                display: flex;
-                justify-content: center;
-                align-content: center;
-                transition: opacity 0.5s ease;
-              }
-  
-              &:hover {
-                transform: scale(1.6);
-  
+            justify-content: space-between;
+            align-items: center;
+    
+            .text-top-right {
+              width: 80px;
+              font-size: 13px;
+              white-space: nowrap;
+              overflow: hidden;
+              text-overflow: ellipsis;
+            }
+    
+            .text-top-left {
+              width: 50px;
+              display: flex;
+              flex-direction: row;
+    
+              .point {
+                width: 12px;
+                height: 12px;
+                margin-left: 4px;
+                border-radius: 50%;
+                text-align: center;
+                transition: transform 0.5s ease;
+    
                 .iconfont {
-                  opacity: 1;
+                  opacity: 0;
+                  font-size: 9px !important;
+                  line-height: 12px;
+                  display: flex;
+                  justify-content: center;
+                  align-content: center;
+                  transition: opacity 0.5s ease;
+                }
+    
+                &:hover {
+                  transform: scale(1.6);
+    
+                  .iconfont {
+                    opacity: 1;
+                  }
                 }
               }
+    
+              .delete {
+                background-color: @delete;
+              }
+    
+              .favorite {
+                background-color: @favorite;
+              }
+    
+              .edit {
+                background-color: @edit;
+              }
             }
-  
-            .delete {
-              background-color: @delete;
-            }
-  
-            .favorite {
-              background-color: @favorite;
-            }
-  
-            .edit {
-              background-color: @edit;
+          }
+    
+          .text-bottom {
+            width: 120px;
+            height: 40px;
+            padding: 5px 0;
+            color: @textFont;
+    
+            .text-direction {
+              font-size: 11px;
+              line-height: 1.3;
+              display: -webkit-box;
+              -webkit-line-clamp: 2;
+              -webkit-box-orient: vertical;
+              overflow: hidden;
+              text-overflow: ellipsis;
             }
           }
         }
-  
-        .text-bottom {
-          width: 120px;
-          height: 40px;
-          padding: 5px 0;
-          color: @textFont;
-  
-          .text-direction {
-            font-size: 11px;
-            line-height: 1.3;
-            display: -webkit-box;
-            -webkit-line-clamp: 2;
-            -webkit-box-orient: vertical;
-            overflow: hidden;
-            text-overflow: ellipsis;
-          }
+    
+        &:hover {
+          transform: scale(1.03);
         }
-      }
-  
-      &:hover {
-        transform: scale(1.03);
       }
     }
   }
